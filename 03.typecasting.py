00a214be39039015e3465e9930d4f39beab06f97
HS_DT <qa.hasansahin@gmail.com>
2024-10-23 09:13:09 -0500

    Typecasting

---
+++ 03.typecasting.py
@@ -0,0 +1,14 @@
+#Typecasting = the process of converting a variable from one data type to another
+#              str(), int(), float(), bool()
+
+name = "John Doe"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(gpa))
+
+gpa = int(gpa)
+
+print(type(gpa))
+print(gpa)
