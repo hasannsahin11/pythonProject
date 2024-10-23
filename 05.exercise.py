344188169392f8645bd8c4e42db25705d91f3897
HS_DT <qa.hasansahin@gmail.com>
2024-10-23 09:29:01 -0500

    Rectangle Area Calculation Exercise

---
+++ 05.exercise.py
@@ -0,0 +1,8 @@
+# Exercise 1 Rectangle Area Calculation
+
+length = float(input("Enter the length"))
+width = float(input("Enter the width"))
+area = length * width
+
+print(f"The area is: {area}cm²")  # For the superscript: NumLock + Alt + 0178
+
